27d1fb87d49a6a1a2bfcdeb6004a84162fee67b7
YvanDroid <yvandroid@outlook.com>
2025-09-29 21:22:54 +0100

    Kitty - kitten start script

---
+++ scripts/kitty-start.sh
@@ -0,0 +1,2 @@
+sleep 10
+kitty +kitten panel --layer=background --edge=right --margin-left 1440 --margin-bottom 0 --margin-top 50 -o background_opacity=0.0 -o window_padding_width=0 --output-name HDMI-A-1 cava
